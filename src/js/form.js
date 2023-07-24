edff64097039d2326e8b7576227f4c5386997242
Yael <yaelmartin1@gmail.com>
2023-07-24 01:35:53 -0300

    funcionamiento del formulario y arreglos de estilos en el about

--- src/js/form.js
+++ src/js/form.js
@@ -1,8 +1,8 @@
-const contactForm = document.querySelector('#contact-form')
+/*const contactForm = document.querySelector('#contact-form')
 
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const { name, email, message } = e.target.elements
 
   e.target.reset()
-})
+})*/
